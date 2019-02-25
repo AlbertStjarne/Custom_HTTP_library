b49497906396cc412be244eb60bff282029b1f9f
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-25 16:11:24 +0100

    create the http DELETE request

--- app.js
+++ app.js
@@ -38,11 +38,21 @@ const data = {
 
 
 // Update Post, PUT
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
-  if(err) {
-        console.log(err);
-      } else {
-        console.log(post);
-      }
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if(err) {
+//         console.log(err);
+//       } else {
+//         console.log(post);
+//       }
 
+// });
+
+
+// Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
 });
