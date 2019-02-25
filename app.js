4148afd3b8c9c3e886e6f8867cf521dcef4727e0
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-25 15:56:54 +0100

    creates the http POST request

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const http = new easyHTTP;
 
 // GET Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+
+// POST
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'THis is a custom post'
+};
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
   if(err) {
-    console.log(err);
-  } else {
-    console.log(posts);
-  }
+        console.log(err);
+      } else {
+        console.log(post);
+      }
 });
+
+
+
