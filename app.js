5d65bbf1db6f9fdbfabd787ed837f1b3cbf146a0
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-26 11:06:25 +0100

    create the GET request

--- app.js
+++ app.js
@@ -1,58 +1,6 @@
 const http = new easyHTTP;
 
-// GET Posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
-
-// Get single Post
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
-
-
-// POST
-
-// Create Data
-const data = {
-  title: 'Custom Post',
-  body: 'THis is a custom post'
-};
-
-// Create Post
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
-//   if(err) {
-//         console.log(err);
-//       } else {
-//         console.log(post);
-//       }
-// });
-
-
-// Update Post, PUT
-// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
-//   if(err) {
-//         console.log(err);
-//       } else {
-//         console.log(post);
-//       }
-
-// });
-
-
-// Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+// Get users
+http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
---
+++ easyhttp2.js
@@ -0,0 +1,12 @@
+class easyHTTP {
+  // Make an HTTP GET request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+
+  }
+}
